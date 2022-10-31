35026cba98edd0262d3750ef70ba7a4c6d62739d
Max Atslega <contact@atslega.network>
2022-10-31 10:34:06 +0100

    add LinGl

---
+++ chapter02/src/main/java/de/atslega/chapter02/task/LinGl.java
@@ -0,0 +1,25 @@
+package de.atslega.chapter02.task;
+
+import java.util.Scanner;
+
+public class LinGl {
+    public static void main(String[] args) {
+        System.out.println("Gleichung ax+b=0");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("A: ");
+        int a = Integer.parseInt(input.nextLine());
+
+        if(a == 0) {
+            System.out.println("Kann nicht gelöst werden");
+            return;
+        }
+
+        System.out.print("B: ");
+        int b = Integer.parseInt(input.nextLine());
+
+        System.out.println("Lösung: x=" + ((-b)/a));
+
+        System.out.println("Programmende LinGl.");
+    }
+}
