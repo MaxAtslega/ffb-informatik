a443fa5ed7e1436088281658bfb9f46fcff04f8a
Max Atslega <contact@atslega.network>
2022-11-07 11:33:39 +0100

    add Koerper

---
+++ chapter02/src/main/java/de/atslega/chapter02/task/Koerper.java
@@ -0,0 +1,43 @@
+package de.atslega.chapter02.task;
+
+import java.util.Scanner;
+
+public class Koerper {
+    public static void main(String[] args){
+        System.out.println("Berechnen von Volumen/Oberfläche div. Körper");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bitte wählen sie  (Z)ylinder, (W)ürfel, (Q)uader, (K)reiskegel oder K(u)gel: ");
+        String wahl = scanner.nextLine();
+
+        System.out.println("Ihre Wahl: "+wahl);
+
+        System.out.print("Kantenlänge in cm ");
+        Double kantenlänge = Double.parseDouble(scanner.nextLine());
+
+        switch (wahl) {
+            case "Z":
+                System.out.println("Zylinder Oberfläche: " + Math.PI * kantenlänge * kantenlänge * kantenlänge);
+                System.out.println("Zylinder Volumen: " + 2 * Math.PI * kantenlänge * (kantenlänge+kantenlänge));
+                break;
+            case "W":
+                System.out.println("Würfel Oberfläche: " + 6 * kantenlänge * kantenlänge);
+                System.out.println("Würfel Volumen: " + kantenlänge * kantenlänge * kantenlänge);
+                break;
+            case "Q":
+                System.out.println("Quader Oberfläche: " + 2 * (3 * kantenlänge * kantenlänge));
+                System.out.println("Quader Volumen: " +  kantenlänge * kantenlänge * kantenlänge);
+                break;
+            case "K":
+                System.out.println("Kreiskegel Oberfläche: " + (Math.PI * kantenlänge * kantenlänge) + (Math.PI * kantenlänge * Math.sqrt(kantenlänge * kantenlänge + kantenlänge * kantenlänge)));
+                System.out.println("Kreiskegel Volumen: " + (Math.PI * kantenlänge * kantenlänge * kantenlänge) / 3);
+                break;
+            case "u":
+                System.out.println("Kugel Oberfläche: " + 4 * Math.PI * kantenlänge * kantenlänge);
+                System.out.println("Kugel Volumen: " + (4 * Math.PI * kantenlänge * kantenlänge * kantenlänge) / 3);
+                break;
+            default:
+                System.out.println("Ihr Wahl existiert nicht.");
+        }
+    }
+}
