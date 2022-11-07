3adf7aa0be4f9929e0251cd22c7bb38b9feba905
Max Atslega <contact@atslega.network>
2022-11-07 10:37:25 +0100

    add MonName

---
+++ chapter02/src/main/java/de/atslega/chapter02/task/MonName.java
@@ -0,0 +1,37 @@
+package de.atslega.chapter02.task;
+
+import java.util.Scanner;
+
+public class MonName {
+    public static void main(String[] args) {
+        System.out.println("Ausgabe des Monatsname");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nummer des Monats (1..12): ");
+        int monat = Integer.parseInt(input.nextLine());
+
+        if (!(monat >= 1 && monat <= 12)) {
+            System.out.println("Der Monat ist Ungültig.");
+        }else {
+            String monatsname = switch (monat) {
+                case 1 -> "Januar";
+                case 2 -> "Februar";
+                case 3 -> "März";
+                case 4 -> "April";
+                case 5 -> "Mai";
+                case 6 -> "Juni";
+                case 7 -> "Juli";
+                case 8 -> "August";
+                case 9 -> "September";
+                case 10 -> "Oktober";
+                case 11 -> "November";
+                case 12 -> "Dezember";
+                default -> "Ungültig";
+            };
+            System.out.println("Der "+ monat + ". Monat heißt "+ monatsname);
+        }
+
+
+        System.out.println("Programmende MonName");
+    }
+}
